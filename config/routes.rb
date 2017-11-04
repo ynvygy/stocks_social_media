8e8140f7695aeeec628d0b410158a9017c2a0e39
ynvygy <lugerand2004@yahoo.com>
2017-11-04 11:27:27 +0200

    added routes and views for friendship

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   	devise_for :users, :controllers => { :registrations => "user/registrations"}
     resources :user_stocks, except: [:edit,:show,:update]
+  	resources :users, only: [:show]
+  	resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: 'welcome#home'
 	get 'my_portfolio', to: "users#my_portfolio"
