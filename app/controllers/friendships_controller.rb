cb69b1fca0e8e0318f9011907af32a5a22f0d510
ynvygy <lugerand2004@yahoo.com>
2017-11-04 17:31:49 +0200

    remove functionality for friends

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,16 @@
+class FriendshipsController < ApplicationController
+
+	def destroy
+		@friendship = current_user.friendships.where(friend_id: params[:id]).first
+		@friendship.destroy
+		respond_to do |format|
+			format.html {redirect_to my_friends_path, notice: "Friend was successfully removed"}
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@user_stocks = @user.stocks
+	end
+
+end
